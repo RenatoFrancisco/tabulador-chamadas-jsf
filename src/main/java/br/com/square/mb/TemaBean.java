d67391fdf5c26be87a612117abdeca45cb0ceb5d
RenatoFrancisco <renatofrancisco.s@gmail.com>
2016-03-14 13:17:13 -0300

    Adicionando TemaBean

---
+++ src/main/java/br/com/square/mb/TemaBean.java
@@ -0,0 +1,36 @@
+package br.com.square.mb;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class TemaBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String tema = "bootstrap"; // Tema padrão.
+
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+
+	// temas disponíveis dentro do JAR
+	public List<String> getTemas() {
+		return Arrays.asList("aristo", "afternoon", "black-tie", "blitzer", "bluesky",
+				"bootstrap", "casablanca", "cupertino", "dark-hive", "dot-luv", "eggplant",
+				"excite-bike", "flick", "glass-x", "hot-sneaks", "humanity",
+				"le-frog", "midnight", "mint-choc", "overcast",
+				"pepper-grinder", "redmond", "rocket", "sam", "smoothness",
+				"south-street", "start", "sunny", "swanky-purse", "trontastic",
+				"ui-darkness", "ui-lightness", "vader");
+	}
+
+}
